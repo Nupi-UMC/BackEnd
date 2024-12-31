92f652f83f7a8cca9376e8c361f926749278aec3
Jimyeong Kim <kjm021221@icloud.com>
2024-12-31 18:17:53 +0900

    first commit

---
+++ src/main/java/com/project/nupibe/NupiBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.nupibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NupiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NupiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/nupibe/NupiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nupibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NupiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
