106c48222f53eea527f07fa8d17e110fb2d74916
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2025-01-18 19:19:50 +0900

    ✨ feat: 가게 조회(단일 가게, 주변 가게, 검색 조회)

--- src/main/java/com/project/nupibe/domain/store/exception/code/StoreErrorCode.java
+++ src/main/java/com/project/nupibe/domain/store/exception/code/StoreErrorCode.java
@@ -10,9 +10,6 @@ public enum StoreErrorCode implements BaseErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "STORE404",
                     "가게를 찾을 수 없습니다."),
-    STORE_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "STORE404_1",
-            "해당하는 가게가 존재하지 않습니다.")
     ;
 
     private final HttpStatus status;
