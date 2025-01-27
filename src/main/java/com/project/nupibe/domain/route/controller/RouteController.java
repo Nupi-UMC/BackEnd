a31a68ea72648f5138abac2f0563b4beebda3952
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2025-01-27 16:15:18 +0900

    ♻️ refactor: 경로 조회 페이지네이션 정렬 기준 추가

--- src/main/java/com/project/nupibe/domain/route/controller/RouteController.java
+++ src/main/java/com/project/nupibe/domain/route/controller/RouteController.java
@@ -2,11 +2,11 @@ package com.project.nupibe.domain.route.controller;
 
 import com.project.nupibe.domain.route.dto.response.RouteDetailResDTO;
 import com.project.nupibe.domain.route.dto.response.RoutePlacesResDTO;
-import com.project.nupibe.domain.route.service.RouteCommandService;
 import com.project.nupibe.domain.route.service.query.RouteQueryService;
-import com.project.nupibe.domain.store.dto.response.StoreResponseDTO;
 import com.project.nupibe.global.apiPayload.CustomResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 public class RouteController {
 
     private final RouteQueryService routeQueryService;
-    private final RouteCommandService routeCommandService;
 
     @GetMapping("/{routeId}")
     @Operation(summary = "경로 상세 조회 API", description = "PathVariable로 보낸 id의 경로를 상세 조회 합니다.")
@@ -36,26 +35,22 @@ public class RouteController {
 
     @GetMapping("/search")
     @Operation(method = "GET", summary = "경로 검색 조회 API", description = "키워드 검색을 통한 경로 조회 API입니다.")
+    @Parameters({
+            @Parameter(name = "cursor", description = "커서 값, 처음이면 0"),
+            @Parameter(name = "query", description = "쿼리 거리순, 북마크순, 추천순")
+    })
     public CustomResponse<RouteDetailResDTO.RoutePageResponse> getRoutesWithQuery(
-            @RequestParam(value="query") String query,
-            @RequestParam(value="cursor", defaultValue = "0") int cursor,
-            @RequestParam(value = "offset", defaultValue = "8") int offset
+            @RequestParam (value="query", defaultValue = "거리순") String query,
+            @RequestParam (value="search") String search,
+            @RequestParam(value="cursor", defaultValue = "0") Long cursor,
+            @RequestParam(value = "offset", defaultValue = "8") int offset,
+            @RequestParam(value="latitude",required = false) Float lat,
+            @RequestParam(value="longitude",required = false) Float lng
     ){
-        RouteDetailResDTO.RoutePageResponse result = routeQueryService.getRoutesWithQuery(query, cursor, offset);
+        float latitude = (lat != null) ? lat : 0.0f;
+        float longitude = (lng != null) ? lng : 0.0f;
+        RouteDetailResDTO.RoutePageResponse result = routeQueryService.getRoutesWithQuery(query, latitude, longitude, search, cursor, offset);
         return CustomResponse.onSuccess(result);
     }
-
-    @PostMapping("{routeId}/bookmark")
-    @Operation(method = "POST", summary = "경로 북마크 API", description = "경로 조회 내 북마크 버튼을 클릭시 작동하는 기능입니다.")
-    public CustomResponse<RouteDetailResDTO.savedDTO> bookmarkRoute(@RequestParam Long memberId, @PathVariable("routeId") Long routeId) {
-        RouteDetailResDTO.savedDTO saved = routeCommandService.bookmarkRoute(memberId, routeId);
-        return CustomResponse.onSuccess(saved);
-    }
-    @PostMapping("{routeId}/like")
-    @Operation(method = "POST", summary = "경로 좋아요 API", description = "경로 조회 내 좋아요 버튼을 클릭시 작동하는 기능입니다.")
-    public CustomResponse<RouteDetailResDTO.savedDTO> likeRoute(@RequestParam Long memberId, @PathVariable("routeId") Long routeId) {
-        RouteDetailResDTO.savedDTO saved = routeCommandService.likeRoute(memberId, routeId);
-        return CustomResponse.onSuccess(saved);
-    }
-
 }
+
--- src/main/java/com/project/nupibe/domain/route/repository/RouteRepository.java
+++ src/main/java/com/project/nupibe/domain/route/repository/RouteRepository.java
@@ -28,11 +28,127 @@ public interface RouteRepository extends JpaRepository<Route,Long> {
     @Query("SELECT r FROM Route r WHERE r.member = :member")
     List<Route> findByMember(@Param("member") Member member);
 
+
     @Query(
-            value = "SELECT * FROM route r WHERE LOWER(r.route_name) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(r.category) LIKE LOWER(CONCAT('%', :query, '%'))",
+            value = """
+            SELECT r.* FROM route r 
+                JOIN route_store rs ON r.id = rs.route_id
+                JOIN store s ON rs.store_id = s.id
+            WHERE (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+                   LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR
+                   LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+            AND rs.order_index = 1
+            ORDER BY ST_Distance(s.coordinates, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) ASC
+            """,
             nativeQuery = true
     )
-    Slice<Route> findByQuery(Pageable pageable, @Param("query") String query);
+    Slice<Route> findBySearchOrderByDistanceDesc(Pageable pageable, @Param("latitude") Float latitude,
+                                                     @Param("longitude") Float longitude, @Param("search") String search);
+
+    @Query(
+            value = """
+                SELECT r.* FROM route r 
+                JOIN route_store rs ON r.id = rs.route_id 
+                JOIN store s ON rs.store_id = s.id 
+                WHERE (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+                       LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR 
+                       LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+                AND rs.order_index = 1
+                AND (
+                    :cursor IS NULL OR 
+                    ST_Distance(s.coordinates1, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) > (
+                        SELECT ST_Distance(s3.coordinates1, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) 
+                        FROM store s3 
+                        JOIN routestore rs3 ON s3.id = rs3.store_id
+                        WHERE rs3.route_id = r.id AND rs3.order_index = 1 
+                        AND s3.id = :cursor
+                    )
+                )
+                ORDER BY 
+                    ST_Distance(s.coordinates1, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) ASC 
+                """,
+            nativeQuery = true
+    )
+    Slice<Route> findBySearchOrderByDistanceWithCursor(
+            @Param("cursor") Long cursor,
+            Pageable pageable,
+            @Param("latitude") Float latitude,
+            @Param("longitude") Float longitude,
+            @Param("search") String search
+    );
+    @Query(
+            value = """
+                    SELECT * FROM route r 
+                    WHERE (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+            LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR 
+            LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+                    ORDER BY r.bookmark_num DESC , r.id DESC 
+                    """,
+            nativeQuery = true
+    )
+    Slice<Route> findBySearchOrderByBOOKMARKNUMAscIdAsc(Pageable pageable, @Param("search") String search);
+    @Query(value = """
+    SELECT r.*, cursorTable.cursor_id FROM route r 
+    JOIN (
+        SELECT r2.id AS cursor_id, CONCAT(
+            LPAD(CAST(r2.bookmark_num AS TEXT), 10, '0'), 
+            LPAD(CAST(r2.id AS TEXT), 10, '0')
+        ) AS cursorValue
+        FROM route r2  
+    ) AS cursorTable ON r.id = cursorTable.cursor_id
+    WHERE(
+            (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+            LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR 
+            LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+        )
+        AND cursorValue < (
+            SELECT CONCAT(
+                LPAD(CAST(r3.bookmark_num AS TEXT), 10, '0'), 
+                LPAD(CAST(r3.id AS TEXT), 10, '0')
+            ) 
+            FROM route r3
+            WHERE r3.id = :cursor
+        )
+    ORDER BY cursorTable.cursorValue DESC 
+    """, nativeQuery = true)
+    Slice<Route> findBySearchOrderByBOOKMARKNUMWithCursor(@Param("storeId") Long cursor, Pageable pageable, @Param("search") String search);
+
+    @Query(
+            value = """
+                    SELECT * FROM route r 
+                    WHERE (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+            LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR 
+            LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+                    ORDER BY r.like_num DESC , r.id DESC 
+                    """,
+            nativeQuery = true
+    )
+    Slice<Route> findBySearchOrderByLikeNumAscIdAsc(Pageable pageable, @Param("search") String search);
+    @Query(value = """
+    SELECT r.*, cursorTable.cursor_id FROM route r 
+    JOIN (
+        SELECT r2.id AS cursor_id, CONCAT(
+            LPAD(CAST(r2.like_num AS TEXT), 10, '0'), 
+            LPAD(CAST(r2.id AS TEXT), 10, '0')
+        ) AS cursorValue
+        FROM route r2
+    ) AS cursorTable ON r.id = cursorTable.cursor_id
+    WHERE(
+            (r.location LIKE LOWER(CONCAT('%', :search, '%')) OR
+            LOWER(r.route_name) LIKE LOWER(CONCAT('%', :search, '%')) OR 
+            LOWER(r.category) LIKE LOWER(CONCAT('%', :search, '%')))
+        )
+        AND cursorValue < (
+            SELECT CONCAT(
+                LPAD(CAST(r3.like_num AS TEXT), 10, '0'), 
+                LPAD(CAST(r3.id AS TEXT), 10, '0')
+            ) 
+            FROM route r3
+            WHERE r3.id = :cursor
+        )
+    ORDER BY cursorTable.cursorValue DESC 
+    """, nativeQuery = true)
+    Slice<Route> findBySearchOrderByLikeNumWithCursor(@Param("cursor") Long cursor, Pageable pageable, @Param("search") String search);
 }
 
 
